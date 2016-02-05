9c067eedd0aa5069e2d54ac42e274760bdb53cb7
soniakhan <sonia.khan@gmail.com>
2016-02-04 16:28:57 -0800

    adad

---
+++ app/views/microposts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @micropost, :id, :title, :body, :user_id, :created_at, :updated_at
---
+++ db/migrate/20160205000826_create_microposts.rb
@@ -0,0 +1,11 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    create_table :microposts do |t|
+      t.text :title
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
