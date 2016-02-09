2d35366b482383ded4d069af4b481de52fd9ecd4
soniakhan <sonia.khan@gmail.com>
2016-02-09 10:50:39 -0800

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'contact' => 'static_pages#contact'
   get 'resume' => 'static_pages#resume'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
 
 
   resources :microposts
