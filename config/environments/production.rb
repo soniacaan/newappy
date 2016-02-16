8c14cd4edae2c1c8cf4812311ca824e3ab2001a3
soniakhan <sonia.khan@gmail.com>
2016-02-15 22:13:09 -0800

    removal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['caansonia'],
-    :password       => ENV['Lahore01'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
