e665e2b8cda038fdc066e8c3b92ab3b239745036
sonia.khan@gmail.com <sonia.khan@gmail.com>
2016-03-04 03:12:42 +0000

    yaml

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -2,7 +2,7 @@
   require 'flickr_fu'
 
 module FlickrHelper  
-
+  require 'yaml'
 
 
     def create
@@ -16,15 +16,15 @@ module FlickrHelper
       current_user.update_attribute :flickr_token, flickr.auth.token.token
     end
    
-    def user_photos(user_id, photo_count = 16)
+    def user_photos(user_id, photo_count = 14)
       @flickr = Flickr.new(File.join(Rails.root, 'config', 'flickr.yml'))
       # now you have full access on the user's data :)
       @flickr.photos.search(:user_id => user_id).values_at(0..(photo_count - 1))
     end
 
-    def render_flickr_sidebar_widget(user_id, photo_count = 16, columns = 8)
+    def render_flickr_sidebar_widget(user_id, photo_count = 14, columns = 7)
       begin
-      photos = user_photos(user_id, photo_count).in_groups_of(8)
+      photos = user_photos(user_id, photo_count).in_groups_of(7)
       render :partial => '/flickr/sidebar_widget', :locals => { :photos => photos }
       rescue Exception
       render :partial => '/flickr/unavailable'
