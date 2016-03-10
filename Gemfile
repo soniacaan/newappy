56f9493d00c5ab1d02598e17379d060061544fb6
sonia.khan@gmail.com <sonia.khan@gmail.com>
2016-03-10 04:54:28 +0000

    changes to carrier.rb

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.2.2'
 gem 'bcrypt', '3.1.7', require: false
 gem 'faker',                '1.4.2'
 gem 'listen', '~> 3.0'
-gem 'rmagick',              '2.15.4'
+gem 'rmagick',              '2.15.4', require: false
 gem 'carrierwave',             '0.10.0'
 gem 'mini_magick',             '3.8.0'
 gem 'fog',                     '1.36.0'
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,15 +9,19 @@ if Rails.env.production?
       :provider         =>     'AWS',
       :aws_access_key_id =>     ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region => ENV['S3_Region'],
+      :aws_region => ENV['S3_Region'],
       :endpoint => 'https://caansonia.s3-website-us-west-2.amazonaws.com',
       :path_style => true
       
     }
+    
+    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+
     config.fog_directory     =  ENV['S3_Bucket']
     config.fog_public    = true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
-    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+    
+    config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 
 
 
