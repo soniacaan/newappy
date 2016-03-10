9bc0577c3f606080646244d04a6fd7a98adcf66a
sonia.khan@gmail.com <sonia.khan@gmail.com>
2016-03-10 06:13:10 +0000

    rmagick from RMagick in uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,6 +3,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
+  include CarrierWave::rmagick
   include CarrierWave::MiniMagick
 
   process resize_to_limit: [620, 371]
