ec87ddb9b3df7219d3586bd96b126a5469e1cff6
sonia.khan@gmail.com <sonia.khan@gmail.com>
2016-03-10 05:23:33 +0000

    updates to carrier

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,19 +9,18 @@ if Rails.env.production?
       :provider         =>     'AWS',
       :aws_access_key_id =>     ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :aws_region => ENV['S3_Region'],
-      :endpoint => 'https://caansonia.s3-website-us-west-2.amazonaws.com',
-      :path_style => true
+      :aws_region => ENV['S3_Region']
+      
       
     }
     
-    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
+   
+    config.cache_dir = "#{Rails.root}/tmp/uploads" 
     config.fog_directory     =  ENV['S3_Bucket']
-    config.fog_public    = true
+    config.fog_public = false
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
+
     
-    config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 
 
 
