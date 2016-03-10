0ca5b9e4a2783bcea76193aab74455c533b6a8d3
sonia.khan@gmail.com <sonia.khan@gmail.com>
2016-03-10 07:02:11 +0000

    keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,14 +9,14 @@ if Rails.env.production?
       :provider         =>     'AWS',
       :aws_access_key_id =>     ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region => ENV['S3_Region']
+      :region => ENV['S3_REGION']
       
       
     }
     
    
     config.cache_dir = "#{Rails.root}/tmp/uploads" 
-    config.fog_directory     =  ENV['S3_Bucket']
+    config.fog_directory     =  ENV['BUCKET_NAME']
     config.fog_public = false
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
 
